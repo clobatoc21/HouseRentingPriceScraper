ddd80447b10e5b24dbee000ae974e1580061f95b
rvfuoc <rvf@uoc.edu>
2021-10-26 19:30:14 +0200

    bucle para los resultados de busqueda

--- WG-Gesucht/wgscraper.py
+++ WG-Gesucht/wgscraper.py
@@ -1,21 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+from time import sleep
+from random import randint
 
-# get data
-data = requests.get(
-    "https://www.wg-gesucht.de/wohnungen-in-Berlin.8.2.1.0.html"
-)
 
-# load data into bs4
-soup = BeautifulSoup(data.text, "html.parser")
+for i in range(10):
+    url = 'https://www.wg-gesucht.de/es/wohnungen-in-Berlin.8.2.1.' + str(i) + '.html'
 
-# locates every listed flat
-flat_data = soup.find_all("div", {"class": "col-sm-8 card_body"})
-for flat in flat_data:
-    name = flat.h3["title"]
-    price = flat.select_one(".col-xs-6, .col-xs-3").get_text(
-        strip=True, separator=" "
-    )
-    size = flat.find("b", text=lambda t: t and "m²" in t)
-    size = size.text if size else "-"
-    print("{:<60} {:<10} {}".format(name[:59], price, size))
+    # get data
+    data = requests.get(url)
+
+    # load data into bs4
+    soup = BeautifulSoup(data.text, "html.parser")
+
+    # locates every listed flat
+    flat_data = soup.find_all("div", {"class": "col-sm-8 card_body"})
+    for flat in flat_data:
+        name = flat.h3["title"]
+        price = flat.select_one(".col-xs-6, .col-xs-3").get_text(
+            strip=True, separator=" "
+        )
+        size = flat.find("b", text=lambda t: t and "m²" in t)
+        size = size.text if size else "-"
+        print("{:<60} {:<10} {}".format(name[:59], price, size))  # añadir para que no pare: .encode("utf-8")
+
+    # sleep to avoid being banned
+    sleep(randint(2,10))
